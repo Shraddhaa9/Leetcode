bf8e8fb7e40f1ac49747ea83fabbf2b9c326723a
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-02-07 19:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (31.34%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -8,6 +8,8 @@
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(len(nums)+1)) - sum(nums)
+'''
         length = len(nums)
         if length == 0:
             return -1
@@ -16,10 +18,14 @@ class Solution:
         
         # enumerate gives index,value from the list
         for i, num in enumerate(nums):
-            # return the missing number
+            # return the missing number, since it's SORTED at somepoint, 
+            # value of index would not match with value, so that we should be able
+            # to get correct value after subtracting 
             if i != num:
                 return num-1
-        
+
             # handles the edge case - case 2; as it's inclusive
             if num == len(nums) - 1:
                 return num+1
+# TC: O(n logn) because of sorting
+'''
