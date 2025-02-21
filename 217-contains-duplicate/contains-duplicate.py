44d13701529a7aad9b1778db4deec009aa9fbbff
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-02-22 00:55:57 +0530

    Time: 43 ms (9.91%) | Memory: 31.5 MB (82.63%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,22 +1,41 @@
-# nums
-# true -> If atleast twice
-# false -> No matching elements found
+## IP: Array
+## OP: True/False (based on duplicate number)
 
-# Corner cases:
-# empty array - return false
-# Single element array - return false
-# -Ve number - allowed
-# Duplicate numbers - allowed [it's THE problem]
+## take 1 number and iterate through the 2nd loop, if number matches
+## TC: O(n^2)
+
+## Sort the array
+## Check two adjacent numbers
+## TC: O(n logn)
+
+## Use hashset
+## Store the number in the set if it's not already there
+## If found return true
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if len(nums) == 1 or len(nums) == 0:
-            return False
-        
-        hs = set()
-        for item in nums:
-            if item in hs:
+        nums.sort()
+        hashset = set()
+        for i in range(len(nums)):
+            if nums[i] in hashset:
                 return True
-            hs.add(item)
-        
-        return False  
+            else:
+                hashset.add(nums[i])
+
+        return False
+
+        ## Sorting approach
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+            
+        # return False
+
+        ## Brute force approach:
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+    
+        # return False
+
