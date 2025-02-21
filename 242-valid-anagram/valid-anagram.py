00bdebe782da777f8566e448cea2e1f5611b4cc2
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-02-22 01:24:20 +0530

    Time: 17 ms (23.19%) | Memory: 18.4 MB (21.99%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,46 @@
+## IP: Two strings
+## OP: True or False (based on whether string is anagram or not)
+
+## Brute force:
+## Check each character from first string present in 2nd string or not
+## TC: O(n^2)
+## SC: O(n) - as we are generating one list
+
+## Better approach:
+## Sort both the strings and check equality
+## If both same then anagram otherwise not
+## TC: O(n logn)
+
+## Optimal approach:
+# 1. array approach
+## Can use array having 26 letters - O(1) space as length of array doesn't increase with the size of the array
+## Define one array having 26 letters;
+## If character found in S; Increment by 1
+## If character found in T; Decrement by 1
+## At the end check if array has any value other than 0, then string NOT anagram, if all 0s, then anagram
+## TC: O(n) - as iterating through single pass, as length of string same, if not same exiting from start only
+## SC: O(1) - as only 26 character length array
+
+#2. Hashmap approach:
+## Iterate through string and store frequency of the character as a value
+## same like array, increment and decrement and get the result
+## TC: O(n) - as iterating through single pass, as length of string same, if not same exiting from start only
+## SC: O(1) - as only 26 character length array
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Better approach: Sorting one
+            return sorted(s) == sorted(t)
+
+        ## BruteForce:
+        # if len(s) != len(t):
+        #     return False
+        
+        # t = list(t)
+        # for char in s:
+        #     if char in t:
+        #         t.remove(char)
+        #     else:
+        #         return False
+        
+        # return True
