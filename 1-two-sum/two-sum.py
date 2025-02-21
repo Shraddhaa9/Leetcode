10ea6ebebc038db86f29e5a2540db016ad8b8fff
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-02-22 02:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (24.52%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -15,6 +15,7 @@
 ## Iterate through the array, take a complement of the current number
 ## and try to see if we have complement number present in the array, not through
 ## LINEAR SEARCH but through BINARY SEARCH
+## BUT HAVE TO BE CAREFUL, AS HAS TO STORE ORIGINAL INDICES AS WELL
 ## TC: O(n logn) + O(logn)
 ## SC: O(1)
 
@@ -27,10 +28,53 @@
 ## TC: O(n) - as we are iterating through entire array
 ## SC: O(n) - as we are defining new hashmap
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            for j in range(i+1, len(nums)):
-                if complement == nums[j]:
-                    return [i, j]
+    ## Optimal approach:
+        nums_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in nums_map:
+                return [nums_map[complement], i]
+            nums_map[num] = i  # Store the number and its index
+        return []
+
+'''
+    # Bruteforce - better:
+    def BinaryS(self, nums, complement) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end)//2
+            if complement == nums[mid]:
+                return mid
+            if complement < nums[mid]:
+                end = mid - 1
+            if complement > nums[mid]:
+                start = mid + 1
+        return -1
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_sorted = sorted((num, i) for i, num in enumerate(nums))  # Sort (value, original index)
+        sorted_nums = [num for num, _ in nums_sorted]
+
+        # Store multiple occurrences of indices
+        original_indices = defaultdict(list)
+        for num, idx in nums_sorted:
+            original_indices[num].append(idx)
+
+        for i in range(len(sorted_nums)):
+            complement = target - sorted_nums[i]
+            idx = self.BinaryS(sorted_nums, complement)
+            if idx != -1 and idx != i:
+                return [original_indices[sorted_nums[i]].pop(0), original_indices[sorted_nums[idx]].pop(0)]
+
+        return []
+'''
+        ## Bruteforce:
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     for j in range(i+1, len(nums)):
+        #         if complement == nums[j]:
+        #             return [i, j]
