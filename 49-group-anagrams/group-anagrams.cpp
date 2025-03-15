9426b05a775ba800ede9e304b5c554dcbb6b523e
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-03-15 06:38:44 +0530

    Time: 12 ms (86.26%) | Memory: 24.9 MB (76.68%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,104 @@
+// IP: Array of strings (str)
+// OP: Array of Array of strings [ANY ORDER, so index/position doesn't matter]
+
+// Anagram - uses same characters in different orders- [combinations of same characters]
+// a, e, t ----> ate, eat, tea
+
+// EMPTY string - return empty string as a group
+
+// Single character - return single character as a group
+
+// QUESTIONS:
+// Do we get same character in a group of strings? i.e. BOON, NOOB, etc?
+// ----> length of 100 characters in a string, and since we have 26 characters in a ABCD,
+//       we can have repeating characters
+// is it only character string? or we can have alphanumeric values as well?
+// ----> it's only character string
+// will single testcase contain always same letter strings? or it will be different?
+// doesn't matter much though
+
+
+// MY APPROACH - inefficient:
+// 1. iterate through the array till end and keep repeating below steps
+// 2. check whether string is valid - non-null
+// ...if it's null, then add that to array of array of string's ith index
+// 3. for non-null strings
+// ...check iteratively for each array of arrya's ith index
+// .......if length doesn't matches
+// ...........continue to next member of array of array's ith
+// .......hashmap - array of arrya's ith match?
+// ...........if hashmap matches
+// ..............we will append array of array's ith
+// ...........if not
+// ..............create new hashmap
+// ..............append to array of array's i+1
+
+// COMMENTS:
+// It has many redundency operations going on, we have to recheck
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        for (const string& str : strs) {
+            string sortedStr = str;  
+            sort(sortedStr.begin(), sortedStr.end()); // Sort characters as the key
+            anagramGroups[sortedStr].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (auto& group : anagramGroups) {
+            result.push_back(group.second);
+        }
+        return result;
+    }
+#if 0
+    unordered_map<char, int> getFrequencyMap(const string& s) {
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+        return freq;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+
+        for (const string& str : strs) {
+            if (str.empty()) { // Handle empty string case
+                result.push_back({str});
+                continue;
+            }
+
+            bool found = false;
+            unordered_map<char, int> currentMap = getFrequencyMap(str);
+
+            // Check if it belongs to an existing group
+            for (auto& group : result) {
+                if (group[0].length() != str.length()) continue; // Length must match
+
+                unordered_map<char, int> existingMap = getFrequencyMap(group[0]);
+                if (existingMap == currentMap) { // Compare frequency maps
+                    group.push_back(str);
+                    found = true;
+                    break;
+                }
+            }
+
+            // If no match, create a new group
+            if (!found) {
+                result.push_back({str});
+            }
+        }
+        
+        return result;  
+    }
+#endif
+};
