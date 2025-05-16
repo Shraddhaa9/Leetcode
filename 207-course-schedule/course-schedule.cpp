6155484ae9085207349c11dff6ebf58abfcf8d3a
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-17 03:47:19 +0530

    Time: 7 ms (51.85%) | Memory: 20.7 MB (18.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,58 @@
 #if 1
 class Solution {
+    // BFS topological
+private:
+    vector<int> computeInDegrees(vector<vector<int>>& adjList) {
+        vector<int> result(adjList.size(), 0);
+        for( auto adj : adjList) {
+            for (auto v : adj) {
+                result[v]++;
+            }
+        }
+        return result;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for(auto preReq : prerequisites) {
+            int a = preReq[0];
+            int b = preReq[1];
+            adjList[b].push_back(a);
+        }
+
+        vector<int> inDegrees = computeInDegrees(adjList);
+        queue<int> q;
+
+        for(int i = 0; i < numCourses; i++) {
+            if (inDegrees[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+        while(!q.empty()) {
+            int size = q.size();
+
+            while(size--) {
+                auto node = q.front();
+                q.pop();
+                count++;
+
+                for(auto neighbour : adjList[node]) {
+                    inDegrees[neighbour]--;
+                    if(inDegrees[neighbour] == 0) {
+                        q.push(neighbour);
+                    }
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+};
+#else
+class Solution {
     // DFS topological
 private:
     bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<bool>& onPath) {
