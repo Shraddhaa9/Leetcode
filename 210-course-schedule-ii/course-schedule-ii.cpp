3033c192d489740a43c94fa234e7646e180294a5
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-17 04:03:35 +0530

    Time: 7 ms (36.12%) | Memory: 19 MB (23.34%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,5 +1,61 @@
 #if 1
 class Solution {
+    // BFS topological
+private:
+    vector<int> computeInDegree(vector<vector<int>>& adjList) {
+        vector<int> inDegrees(adjList.size(), 0);
+        for (auto node : adjList) {
+            for (auto v : node) {
+                inDegrees[v]++;
+            }
+        }
+
+        return inDegrees;
+    }
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for(auto preReq : prerequisites) {
+            int a = preReq[0];
+            int b = preReq[1];
+            adjList[b].push_back(a);
+        }
+
+        vector<int> inDegrees = computeInDegree(adjList);
+        vector<int> schedule;
+        queue<int> q;
+
+        for(int i = 0; i < numCourses; i++) {
+            if (inDegrees[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int size = q.size();
+
+            while(size--) {
+                auto node = q.front();
+                q.pop();
+                schedule.push_back(node);
+
+                for(auto neighbour : adjList[node]) {
+                    inDegrees[neighbour]--;
+                    if (inDegrees[neighbour] == 0) {
+                        q.push(neighbour);
+                    }
+                }
+            }
+        }
+
+        if (numCourses == schedule.size()) return schedule;
+
+        return {};
+    }
+};
+#else
+class Solution {
     // DFS topological
 private:
     bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<bool>& onPath, stack<int>& s) {
