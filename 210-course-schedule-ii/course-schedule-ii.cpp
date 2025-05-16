3fe40922bf63bd26dac56b2ec2ba92a648c662bb
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-17 01:01:04 +0530

    Time: 5 ms (46.70%) | Memory: 19.1 MB (16.94%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& visited, vector<bool>& onPath, stack<int>& s) {
+        visited[node] = true;
+        onPath[node] = true;
+
+        for(auto neighbour : adjList[node]) {
+            if(!visited[neighbour]) {
+                if (!dfs(neighbour, adjList, visited, onPath, s)) {
+                    return false;
+                }
+            } else if (onPath[neighbour]) {
+                    return false; // found a cycle
+            }
+        }
+
+        onPath[node] = false;
+        s.push(node);
+        return true;
+    }
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for(auto preReq : prerequisites) {
+            int a = preReq[0];
+            int b = preReq[1];
+            adjList[b].push_back(a);
+        }
+
+        vector<bool> visited(numCourses, false);
+        vector<bool> onPath(numCourses, false);
+        stack<int> s;
+
+        for(int i = 0; i <numCourses; i++) {
+            if(!visited[i]) {
+                if(!dfs(i, adjList, visited, onPath, s)) {
+                    return {};
+                }
+            }
+        }
+
+        vector<int> schedule;
+        while(!s.empty()) {
+            schedule.push_back(s.top());
+            s.pop();
+        }
+
+        return schedule;
+    }
+};
