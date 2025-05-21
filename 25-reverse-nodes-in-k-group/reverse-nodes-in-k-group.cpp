caaa258affcfea0642ccd426cf866300a6a08ba8
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-21 08:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (70.08%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -24,6 +24,43 @@ private:
         return prev;
     }
 
+#if 1
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *ptr = head;
+        ListNode *ktail = nullptr;
+        ListNode *newHead = nullptr;
+
+        while(ptr != nullptr) {
+            int count = 0;
+            ListNode *khead = ptr;
+            while(count < k && ptr != nullptr) {
+                ptr = ptr->next;
+                count++;
+            }
+
+            if (count == k) {
+                ListNode *revHead = reverseLinkedList(khead, k);
+
+                if (newHead == nullptr) {
+                    newHead = revHead;
+                }
+
+                if (ktail != nullptr) {
+                    ktail->next = revHead;
+                }
+
+                ktail = khead;
+            } else {
+                if (ktail != nullptr) {
+                    ktail->next = khead;
+                }
+            }
+        }
+
+        return newHead == nullptr ? head : newHead;
+    }
+#else
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         int count = 0;
@@ -42,4 +79,5 @@ public:
 
         return head;   
     }
+#endif
 };
