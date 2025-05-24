8fe32e33af650762d52d63f10e504c09b0656024
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-25 03:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 71.5 MB (97.70%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            int sum = sum + nums[i];
+
+            maxSum = max(maxSum, sum);
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+};
+
+
+// generate all subarray of input array
+// add the elements
+// compare the summed result
+// the subset which gave the larget sum is the answer
+
+// corner case:
+// 1 or 2 members in a array
