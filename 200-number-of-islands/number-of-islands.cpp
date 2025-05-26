cadc8eefe7ec23b4d47c26d45064477985da3e72
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-27 05:28:36 +0530

    Time: 23 ms (84.12%) | Memory: 16 MB (99.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,3 +1,42 @@
+#if 1
+class Solution {
+private:
+    void explore (int row, int col, vector<vector<char>> &grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if(row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '0') return;
+
+        grid[row][col] = '0';
+
+        explore(row - 1, col, grid);
+        explore(row, col - 1, grid);
+        explore(row + 1, col, grid);
+        explore(row, col + 1, grid);
+
+        return;
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    islands++;
+                    explore(i, j, grid);
+                }
+            }
+        }
+        return islands;
+    }
+};
+#else
+
+#endif
+#if 0
 class Solution {
 private:
     bool explore(vector<vector<char>>& grid, int r, int c, set<pair<int, int>>& visited) {
@@ -34,3 +73,4 @@ public:
         return islands;
     }
 };
+#endif
