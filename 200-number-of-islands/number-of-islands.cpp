ed8e6e5864f15b4b52ee3f617eb87ab608221dd2
Shraddha Rakholiya <102873671+Shraddhaa9@users.noreply.github.com>
2025-05-27 05:46:37 +0530

    Time: 18 ms (97.68%) | Memory: 16 MB (95.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -34,7 +34,45 @@ public:
     }
 };
 #else
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        if (m == 0) return 0;
 
+        int n = grid[0].size();
+        int islands = 0;
+
+        queue<pair<int, int>> q;
+        vector<pair<int, int>> dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    islands++;
+                    grid[i][j] = '0';
+                    q.push({i, j});
+
+                    while(!q.empty()) {
+                        auto [r, c] = q.front();
+                        q.pop();
+                        for(auto [dr, dc] : dirs) {
+                            nr = r + dr;
+                            nc = c + dc;
+
+                            if(nr <0 || nr >= m || nc < 0 || nc >= n) continue;
+                            if(grid[nr][nc] == '0') continue;
+
+                            grid[nr][nc] = '0';
+                            q.push({nr, nc});
+                        }
+                    }
+                }
+            }
+        }
+        return islands;
+    }
+};
 #endif
 #if 0
 class Solution {
